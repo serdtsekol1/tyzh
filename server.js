28b5d0a037b922355ce54baa889a67e8ece02dcd
biletboh <biletskyboh@gmail.com>
2020-08-19 21:31:22 +0300

    changed port to 3000 and removed console.logs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch   = require('node-fetch');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const path = require('path');
 const fs = require('fs')
 
@@ -29,7 +29,6 @@ app.get('/Publications/:category/:id', function(request, response) {
   const category = request.params.category;
   const id = request.params.id;
   let articleInfoJson={};
-  console.log(id);
   const filePath = path.resolve(__dirname, './build', 'index.html');
   fetch(`https://new.tyzhden.ua/api/publications/${id}`)
     .then(res => res.json())
@@ -40,7 +39,6 @@ app.get('/Publications/:category/:id', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, articleInfoJson.title);
     data = data.replace(/\$OG_DESCRIPTION/g, articleInfoJson.abstract);
@@ -67,7 +65,6 @@ app.get('/Columns/:id', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, columnInfoJson.title);
     data = data.replace(/\$OG_DESCRIPTION/g, columnInfoJson.abstract);
@@ -84,7 +81,6 @@ app.get('/News/:id', function(request, response) {
   
   const id = request.params.id;
   let newsInfoJson={};
-  console.log(id);
   const filePath = path.resolve(__dirname, './build', 'index.html');
   fetch(`https://new.tyzhden.ua/api/news/${id}`)
     .then(res => res.json())
@@ -95,7 +91,6 @@ app.get('/News/:id', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, newsInfoJson.title);
     data = data.replace(/\$OG_DESCRIPTION/g, newsInfoJson.abstract);
@@ -139,7 +134,6 @@ app.get('/Magazine/:id', function(request, response) {
   
   const id = request.params.id;
   let magazineInfoJson={};
-  console.log(id);
   const filePath = path.resolve(__dirname, './build', 'index.html');
   fetch(`https://new.tyzhden.ua/api/magazine/${id}`)
     .then(res => res.json())
@@ -167,7 +161,6 @@ app.get('/Author/:id', function(request, response) {
   
   const id = request.params.id;
   let infoJson={};
-  console.log(id);
   const filePath = path.resolve(__dirname, './build', 'index.html');
   fetch(`https://new.tyzhden.ua/api/authors/page/${id}`)
     .then(res => res.json())
@@ -178,7 +171,6 @@ app.get('/Author/:id', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, infoJson.fullname2ua);
     data = data.replace(/\$OG_DESCRIPTION/g, infoJson.fullname2ua);
@@ -202,7 +194,6 @@ app.get('/Publications', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, "Ексклюзивні статті зарубіжних партнерів, статті міжнародних експертів, ключові події в Європі, Росії, Америці, на Близькому Сході, новини в світі");
     data = data.replace(/\$OG_DESCRIPTION/g, "Ексклюзивні статті зарубіжних партнерів, статті міжнародних експертів, ключові події в Європі, Росії, Америці, на Близькому Сході, новини в світі");
@@ -225,7 +216,6 @@ app.get('/Publications/page=:page', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, "Ексклюзивні статті зарубіжних партнерів, статті міжнародних експертів, ключові події в Європі, Росії, Америці, на Близькому Сході, новини в світі");
     data = data.replace(/\$OG_DESCRIPTION/g, "Ексклюзивні статті зарубіжних партнерів, статті міжнародних експертів, ключові події в Європі, Росії, Америці, на Близькому Сході, новини в світі");
@@ -248,7 +238,6 @@ app.get('/Publications/:category', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, "Ексклюзивні статті зарубіжних партнерів, статті міжнародних експертів, ключові події в Європі, Росії, Америці, на Близькому Сході, новини в світі");
     data = data.replace(/\$OG_DESCRIPTION/g, "Ексклюзивні статті зарубіжних партнерів, статті міжнародних експертів, ключові події в Європі, Росії, Америці, на Близькому Сході, новини в світі");
@@ -271,7 +260,6 @@ app.get('/Publications/:category/page=:page', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, "Ексклюзивні статті зарубіжних партнерів, статті міжнародних експертів, ключові події в Європі, Росії, Америці, на Близькому Сході, новини в світі");
     data = data.replace(/\$OG_DESCRIPTION/g, "Ексклюзивні статті зарубіжних партнерів, статті міжнародних експертів, ключові події в Європі, Росії, Америці, на Близькому Сході, новини в світі");
@@ -295,7 +283,6 @@ app.get('/News/page=:page', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, `Новини в Україні, економіка, політика, культура, новини в світі, об'єктивно та ексклюзивно про головні події в Україні та світі`);
     data = data.replace(/\$OG_DESCRIPTION/g, "Новини в Україні, економіка, політика, культура, новини в світі, об'єктивно та ексклюзивно про головні події в Україні та світі");
@@ -315,7 +302,6 @@ app.get('/News', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, `Новини в Україні, економіка, політика, культура, новини в світі, об'єктивно та ексклюзивно про головні події в Україні та світі`);
     data = data.replace(/\$OG_DESCRIPTION/g, "Новини в Україні, економіка, політика, культура, новини в світі, об'єктивно та ексклюзивно про головні події в Україні та світі");
@@ -335,7 +321,6 @@ app.get('/Columns/page=:page', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, "Думки впливових людей, колонки закордонних експертів");
     data = data.replace(/\$OG_DESCRIPTION/g, "Думки впливових людей, колонки закордонних експертів");
@@ -355,7 +340,6 @@ app.get('/Columns', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, "Думки впливових людей, колонки закордонних експертів");
     data = data.replace(/\$OG_DESCRIPTION/g, "Думки впливових людей, колонки закордонних експертів");
@@ -375,7 +359,6 @@ app.get('/Gallery', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, "Фоторепортажі, фотогалереї, фото дня, новини у фото, подорожі у фото");
     data = data.replace(/\$OG_DESCRIPTION/g, "Фоторепортажі, фотогалереї, фото дня, новини у фото, подорожі у фото");
@@ -395,7 +378,6 @@ app.get('/Gallery/page=:page', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, "Фоторепортажі, фотогалереї, фото дня, новини у фото, подорожі у фото");
     data = data.replace(/\$OG_DESCRIPTION/g, "Фоторепортажі, фотогалереї, фото дня, новини у фото, подорожі у фото");
@@ -415,7 +397,6 @@ app.get('/Magazines', function(request, response) {
     if (err) {
       return console.log(err);
     }
-    console.log(articleInfoJson);
     // replace the special strings with server generated strings
     data = data.replace(/\$OG_TITLE/g, "Випуски журналу &laquo;Український тиждень&raquo;, &laquo;Український тиждень&raquo; онлайн-версія, &laquo;Український тиждень&raquo; онлайн");
     data = data.replace(/\$OG_DESCRIPTION/g, "Випуски журналу &laquo;Український тиждень&raquo;, &laquo;Український тиждень&raquo; онлайн-версія, &laquo;Український тиждень&raquo; онлайн");
