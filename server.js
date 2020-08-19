ed1d56b6f914c37855a8c56e084c623bb5ff0a33
dali <daryaliha@gmail.com>
2020-08-19 21:50:23 +0300

    added og_url tag

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch   = require('node-fetch');
 const app = express();
-const port = process.env.PORT || 5011;
+const port = process.env.PORT || 3000;
 const path = require('path');
 const fs = require('fs')
 
