24fa12f91ae4e4beba7fc41a775417a645ff600b
dali <daryaliha@gmail.com>
2020-08-19 22:34:37 +0300

    deleted logs inside then promise

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch   = require('node-fetch');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const path = require('path');
 const fs = require('fs')
 
