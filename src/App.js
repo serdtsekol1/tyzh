d67ab2f49192fd7792052c6fdcff8815b9d27161
kovalenko910 <kovalenko910@gmail.com>
2020-10-07 21:56:12 +0300

    added new Google Tag with the History Change trigger

--- src/App.js
+++ src/App.js
@@ -24,17 +24,9 @@ import ReactGA from 'react-ga';
 import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory();
-history.listen(location => {
-  // ReactGA.set({ page: location.pathname }); // Update the user's current page
-  // ReactGA.pageview(location.pathname); // Record a pageview for the given page
-    if (window.ga) {
-        window.ga('send', 'pageview', location.pathname);
-    }
-});
 
 function App() {
   //ReactGA.initialize('UA-54516992-1');
-    window.ga('create', 'UA-51335057-1', 'auto');
   return (
     <Router history={history}>
       <ScrollToTop/>
