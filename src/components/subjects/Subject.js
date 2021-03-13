eca29ade8c8b5e085c4428fab5e7a9fe7d219728
Bohdan Biletskyi <biletskyboh@gmail.com>
2021-03-14 01:41:57 +0200

    added subjects basic template and route

---
+++ src/components/subjects/Subject.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect}  from "react";
+import { useHistory } from "react-router-dom";
+
+import axios from 'axios';
+import config from 'react-global-configuration';
+import SkeletonPublication from "../loading_skeletons/SkeletonPublication";
+
+import SubjectTemplate from "./SubjectTemplate";
+
+
+import "../common/css/post.scss";
+
+
+function Subject({ match }) {
+  const [subject, setSubject] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  let history = useHistory();
+  let subjectComponent;
+
+  useEffect (() => {
+    setLoading(true);
+
+    const fetchData = async () => {
+      let apiUrl = `${config.get("apiDomain")}/subjects/${match.params.id}`;
+      await axios.get(apiUrl)
+        .then(res => {
+          setSubject(res.data);
+          setLoading(false);
+        })
+        .catch(err => history.push('/page-not-found/'));
+    };
+
+    fetchData();
+
+  }, [match.params.id, history]);
+
+  subjectComponent = <SubjectTemplate subject={subject}/>;
+
+  return (
+    <div>
+    {loading && <SkeletonPublication article={true}/>}
+    {!loading && <div>{subjectComponent}</div>}
+    </div>
+  );
+}
+
+export default Subject;
---
+++ src/components/subjects/SubjectTemplate.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import PublicationAbstract from "../common/PublicationAbstract";
+
+import "../common/css/post.scss";
+
+
+function SubjectTemplate(props) {
+  return (
+    <div>Subject</div>
+  );
+}
+
+export default SubjectTemplate;
