058e0477403ad3b1b8b0759d840fb7e59380c5ea
Bohdan Biletskyi <biletskyboh@gmail.com>
2021-04-09 00:19:51 +0300

    added basic layout to modal window

--- src/components/common/PatreonPopup.js
+++ src/components/common/PatreonPopup.js
@@ -1,25 +1,29 @@
 import React from "react";
 import Modal from 'react-modal';
+import Button from "../common/Button";
+
+import "../common/css/_modals.scss";
 
 
 function PatreonPopup(props) {
 
   const customStyles = {
     content : {
-      top                   : '50%',
-      left                  : '50%',
-      right                 : 'auto',
-      bottom                : 'auto',
-      marginRight           : '-50%',
-      transform             : 'translate(-50%, -50%)'
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      maxWidth: '900px',
+      border: 'none',
+      boxShadow: '0px 0px 64px rgba(0, 0, 0, 0.12)',
     }
   };
 
   Modal.setAppElement('#root')
 
-  var subtitle;
-
-  const [modalIsOpen,setIsOpen] = React.useState(false);
+  const [modalIsOpen, setIsOpen] = React.useState(false);
 
   function openModal() {
     setIsOpen(true);
@@ -27,7 +31,6 @@ function PatreonPopup(props) {
 
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
   }
 
   function closeModal(){
@@ -42,18 +45,21 @@ function PatreonPopup(props) {
           onAfterOpen={afterOpenModal}
           onRequestClose={closeModal}
           style={customStyles}
-          contentLabel="Example Modal"
+          contentLabel="Patreon Modal"
         >
-          <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
           <button onClick={closeModal}>close</button>
-          <div>I am a modal</div>
-          <form>
-            <input />
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
-          </form>
+          <div className="model-image">
+          </div>
+          <div className="body-modal">
+            <h2 className="header-modal">Дмитро Крапивенко, головний
+редактор</h2>
+            <div className="body-text">
+              <p>Ми будемо вдячні кожному, хто підтримає «Український тиждень» у цей складний час. Зробити це доволі просто: передплатити журнал або зробити пожертву у наш гонорарний фонд.</p>
+            </div>
+            <a href="https://www.patreon.com/ukrainianweek">
+              <Button title="Підтримати на Патреон" redButton={true}/>
+            </a>
+          </div>
         </Modal>
       </div>
   );
