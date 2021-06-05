0af3a9570a6d7599c4b4aa5c1892f7c099ddd83c
Bohdan Biletskyi <biletskyboh@gmail.com>
2021-06-05 14:25:36 +0300

    added basic routes for Authors

---
+++ pages/Author/[id]/Publications.js
@@ -0,0 +1,25 @@
+import Layout from '../../../components/layout'
+import Head from 'next/head'
+
+
+export default function AuthorPublication({ data }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{data.fullname2ua}</title>
+      </Head>
+    </Layout>
+  )
+}
+
+
+export async function getServerSideProps(context) {
+  let apiUrl = `${process.env.apiDomain}authors/page/${context.params.id}/`
+  const res = await fetch(apiUrl)
+  if (res.status == 200) {
+    const data = await res.json()
+    return { props: { data } }
+  } else {
+    return { notFound: true }
+  }
+}
