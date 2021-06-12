0d4b19707cd532cda0f6a59fa81d1d2a9bca1e6b
kovalenko910 <kovalenko910@gmail.com>
2021-06-12 18:25:52 +0300

    Fixed the issues of the MoxTV ad

--- components/fragments/MoxTV.js
+++ components/fragments/MoxTV.js
@@ -9,37 +9,33 @@ function MoxTV(props) {
   function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
-  const [randomNum,setRandonNum] = useState(getRandomInt(1000000000));
-  const [moxTV,setMoxTV] = useState(document.getElementById(`${props.moxTV_id}_wrap`));
-
     
   
   useEffect(() => {
-    setMoxTV(document.getElementById(`${props.moxTV_id}_wrap`));
-    
+    let moxTV;
+    console.log(moxTV);
+    moxTV = document.getElementById(`${props.moxTV_id}_wrap`);
+    console.log(moxTV);
 
-
+    console.log(props.moxTV_id);
     if (moxTV) {
       console.log(moxTV);
-    //   if(!moxTV.querySelector("#moxTV")) {
 
-    //   }
       const script2 = document.createElement("script");
       script2.type = "text/javascript";
       script2.id="moxTV";
       script2.innerHTML = ` (function(window, document, undefined) {
         var script_tag = document.createElement('script');
         script_tag.src = 'https://ad.mox.tv/mox/mwayss_invocation.min.js?pzoneid=19&height=250&width=300&tld=tyzhden.ua&ctype=div';
-        var container = document.querySelectorAll('[data-id=_mwayss-${randomNum}]')[0];    
+        var container = document.querySelectorAll('[data-id=_mwayss-6868686868]')[0];    
         container.setAttribute('id', (container.getAttribute('data-id')+(new Date()).getTime()));
         container.removeAttribute('data-id');
         container.parentNode.insertBefore(script_tag, container);
     })(window, document);`;
-          moxTV.appendChild(script2); 
-    // }
-  }
+          moxTV.appendChild(script2);
+    }
     
-  },[moxTV]);
+  },[]);
  
   
   return (
@@ -47,7 +43,7 @@ function MoxTV(props) {
 
 
     <div id={`${props.moxTV_id}_wrap`} className="image-banner">
-      <div data-id={`_mwayss-${randomNum}`}></div>
+      <div data-id={`_mwayss-6868686868`}></div>
     </div>)
 
 }
