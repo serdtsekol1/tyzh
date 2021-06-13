da031c81a5c44460048c5f1ed2f185f683c1754e
kovalenko910 <kovalenko910@gmail.com>
2021-06-13 19:25:59 +0300

    Added new google code

--- components/layout.js
+++ components/layout.js
@@ -11,6 +11,21 @@ export default function Layout({ children }) {
   return (
     <div className="App">
       <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-51335057-1"
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-51335057-1', { page_path: window.location.pathname });
+            `,
+          }}
+        />
     
       </Head>
       <Navbar />
